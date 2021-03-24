b78188a5067baac4f21588d3c1c60a3398640bb8
Sarma Krovi <sarmakdvsr@gmail.com>
2021-03-23 23:43:44 -0400

    Fix PartA

--- java/src/main/java/FileLoader.java
+++ java/src/main/java/FileLoader.java
@@ -1,8 +1,8 @@
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.StructType;
 
 public class FileLoader {
   private FileLoader() {
@@ -11,18 +11,16 @@ public class FileLoader {
 
   public static Dataset<?> loadData(final JavaSparkContext sc, final SQLContext sqlContext) {
     JavaRDD<Record> rdd = sc.textFile("gbooks")
-      .map(new Function<String, Record>() {
-        @Override
-        public Record call(String line) throws Exception {
-          String[] parts = line.split("\t");
-          Record record = new Record();
-          record.setWord(parts[0]);
-          record.setCount1(Integer.parseInt(parts[1]));
-          record.setCount2(Integer.parseInt(parts[2]));
-          record.setCount3(Integer.parseInt(parts[3]));
-          return record;
-        }
+      .map(line -> {
+        String[] parts = line.split("\t");
+        Record record = new Record();
+        record.setWord(parts[0]);
+        record.setCount1(Integer.parseInt(parts[1]));
+        record.setCount2(Integer.parseInt(parts[2]));
+        record.setCount3(Integer.parseInt(parts[3]));
+        return record;
       });
-    return sqlContext.applySchema(rdd, Record.class);
+
+    return sqlContext.createDataFrame(rdd, Record.class);
   }
 }
--- java/src/main/java/MP3_PartA.java
+++ java/src/main/java/MP3_PartA.java
@@ -3,6 +3,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 
 public final class MP3_PartA {
 
@@ -11,15 +12,30 @@ public final class MP3_PartA {
       .builder()
       .appName("MP3")
       .getOrCreate();
-    JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-    SQLContext sqlContext = new SQLContext(sc);
-    Dataset<?> dataset = FileLoader.loadData(sc, sqlContext);
-    dataset.createOrReplaceTempView("gbooks");
-    Dataset<Row> result = spark.sql("SELECT word, count1, count2, count3 from gbooks");
-    result.printSchema();
+
+
+    try (JavaSparkContext sc = new JavaSparkContext(spark.sparkContext())) {
+      StructType schema = new StructType()
+        .add("word", "string", false)
+        .add("count1", "integer", false)
+        .add("count2", "integer", false)
+        .add("count3", "integer", false);
+
+      // SQLContext sqlContext = new SQLContext(sc);
+      // Dataset<?> dataset = FileLoader.loadData(sc, sqlContext);
+      Dataset<Row> dataset = spark.read().schema(schema).option("delimiter","\t").load("gbooks");
+      dataset.createOrReplaceTempView("gbooks");
+      dataset.printSchema();
+      sc.stop();
+    }
 
     // Finish up
     spark.stop();
-    sc.stop();
   }
 }
+
+// Part A: Your_output_contains:_3_incorrect_lines_in_our_test.Please_try_again!      ;
+// Part B: Congrats!_All_lines_are_correct!      ;
+// Part C: Congrats!_All_lines_are_correct!      ;
+// Part D: Congrats!_All_lines_are_correct!      ;
+// Part E: Congrats!_All_lines_are_correct!      ;
